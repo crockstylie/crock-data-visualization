9765f6caa21ceaad0dbb3d9f9698af238479cae0
HORY Antoine <communication@apgenes.com>
2025-07-02 14:33:08 +0200

    Setting the begining of the dev environment

---
+++ crock-data-visualizer.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Universal Data Visualizer by Crock
+Plugin URI: https://antoinehory.fr
+Description: A WordPress plugin that helps people importing data in the Back Office and showing it in the Front Office
+Version: 1.0
+Author: Antoine (Crock) HORY
+Author URI: https://antoinehory.fr
+License: GPL v3
+*/
